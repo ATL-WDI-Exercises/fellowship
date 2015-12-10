b271c7dd499ce4a111ee06fd4ea4f2821b83e3c1
Chad Christensen <chadthecoder@gmail.com>
2015-12-09 23:07:27 -0500

    almost done

--- fellowship.js
+++ fellowship.js
@@ -37,25 +37,41 @@ var fellowship = {
   makeHobbits: function(hobbits) {
     // display an unordered list of hobbits in the shire
     var ul = document.createElement("ul");
-    for ( var i = 0; i < hobbits.length; i++) {
+    for (var i = 0; i < hobbits.length; i++) {
       var li = document.createElement("li");
       li.innerHTML = hobbits[i];
       // give each hobbit a class of hobbit
       li.setAttribute("class", "hobbit");
       ul.appendChild(li);
     }
+    document.body.appendChild(ul);
   },
   keepItSecretKeepItSafe: function() {
     // create a div with an id of 'the-ring'
+    var div = document.createElement("div");
+    div.setAttribute("id", "the-ring");
     // add the ring as a child of Frodo
+    var frodo = document.getElementsByClassName("hobbit")[0];
+    frodo.appendChild(div);
   },
   makeBuddies: function(buddies) {
     // create an aside tag
+    var aside = document.createElement("aside");
     // display an unordered list of buddies in the aside
+    var ul = document.createElement("ul");
+    for (var i = 0; i < buddies.length; i++) {
+      var li = document.createElement("li");
+      li.innerHTML = buddies[i];
+      ul.appendChild(li);
+    }
+    aside.appendChild(ul);
     // insert your aside before rivendell
+    var lands = document.getElementById("middle-earth");
+    lands.insertBefore(aside, lands.childNodes[1]);
   },
   beautifulStranger: function() {
     // change the buddy 'Strider' textnode to "Aragorn"
+
   },
   forgeTheFellowShip: function() {
     // move the hobbits and the buddies to Rivendell
