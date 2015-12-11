d7f9de823d94f541324516b88239ec9959e69ee6
Nick Touchette <touchette@gmail.com>
2015-12-10 20:59:44 -0500

    finished application using javascript

--- fellowship.js
+++ fellowship.js
@@ -17,7 +17,7 @@ var fellowship = {
     "The Shire", "Rivendell", "Mordor"
   ],
   makeMiddleEarth: function(lands) {
-    // Set parent element to add into
+    // Set parent element with ID of middle-earth
     var parent = document.getElementById("middle-earth");
 
     // Create article element
@@ -56,32 +56,91 @@ var fellowship = {
     parent.insertBefore(ul, parent.children[1]);
   },
   keepItSecretKeepItSafe: function() {
-      // set parent element
-      var parent = document.querySelector("li.hobbit");
+    // set first list item w/ class of hobbit as parent element
+    var parent = document.querySelector("li.hobbit");
 
-      // Create the ring
-      var theRing = document.createElement("div");
+    // Create the ring
+    var theRing = document.createElement("div");
 
-      // Give ring id of "the-ring"
-      var ringId = document.createAttribute("id");
-      ringId.value = "the-ring";
-      theRing.setAttributeNode(ringId);
+    // Give ring id of "the-ring"
+    var ringId = document.createAttribute("id");
+    ringId.value = "the-ring";
+    theRing.setAttributeNode(ringId);
 
-      parent.insertBefore(theRing, parent.children[1]);
+    parent.insertBefore(theRing, parent.children[1]);
   },
   makeBuddies: function(buddies) {
-    // create an aside tag
-    // display an unordered list of buddies in the aside
-    // insert your aside before rivendell
+    // set parent to article
+    var parent = document.querySelector("#middle-earth article");
+
+    // create aside and ul elements
+    var aside = document.createElement("aside");
+    var ul = document.createElement("ul");
+
+    buddies.forEach(function(buddy) {
+      // create li element
+      var li = document.createElement("li");
+      // get name of buddy and apply to text node
+      var text = document.createTextNode(buddy);
+      // append ul < li < text
+      ul.appendChild(li).appendChild(text);
+    });
+    // aside < ul
+    aside.appendChild(ul);
+    // append aside to DOM
+    parent.insertBefore(aside, parent.children[3]);
+
+
   },
   beautifulStranger: function() {
-    // change the buddy 'Strider' textnode to "Aragorn"
+    // get position of strider in array
+    var striderLocation = fellowship.buddies.indexOf('Strider');
+
+    // find strider in DOM
+    var strider = document.querySelectorAll('aside ul li')[striderLocation];
+
+    // set striders parent
+    var parent = strider.parentNode;
+
+    // create li element
+    var li = document.createElement("li");
+    var name = document.createTextNode("Aragorn");
+    li.appendChild(name);
+    parent.replaceChild(li, strider);
   },
   forgeTheFellowShip: function() {
-    // move the hobbits and the buddies to Rivendell
-    // create a new div called 'the-fellowship'
-    // add each hobbit and buddy one at a time to 'the-fellowship'
-    // after each character is added make an alert that they have joined your party
+    // get hobbits by class name
+    var hobbits = document.getElementsByClassName("hobbit");
+    // get location of rivendell
+    var rivendell = document.querySelector("#middle-earth article aside ul");
+
+    // iterate over hobbits to add to rivendell
+    for (var i = hobbits.length - 1; i >= 0; i--) {
+      rivendell.appendChild(hobbits[i]);
+    }
+
+    // create div, h1, header text, and ul element
+    var parent = document.querySelector("#middle-earth article");
+    var div = document.createElement("div");
+    var h1 = document.createElement("h1");
+    var h1text = document.createTextNode("the-fellowship");
+    var ul = document.createElement("ul");
+
+    // combine header text with h1
+    h1.appendChild(h1text);
+
+    // get all buddies and hobbits
+    var all = document.getElementsByTagName("li");
+    parent.appendChild(div).appendChild(h1);
+    div.appendChild(ul);
+
+    // loop and append to fellowship ul
+    for (var i = all.length - 1; i >= 0; i--) {
+      ul.appendChild(all[i]);
+      if (i === 0) {
+        alert("Everyone has joined the fellowship!");
+      }
+    }
   }
 }
 
